ce2b019ea8f9087ac331bf7905b01cace535af39
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-08-10 20:09:50 +0200

    UPDATE

--- solution.js
+++ solution.js
@@ -1,18 +1,17 @@
 var MongoClient = require('mongodb').MongoClient
-var url = 'mongodb://localhost:27017/learnyoumongo';
-var user = {
-	firstName: process.argv[2],
-	lastName: process.argv[3]
-}
+var dbName = process.argv[2];
+var url = 'mongodb://localhost:27017/' + dbName;
 
 MongoClient.connect(url, function(err, db) {
 	if (err) throw err;
 	db.collection('users')
-		.insert( user, function(err, results) {
+	.update(
+		{ username: "tinatime" },
+		{ $set: { age: 40 } },
+		function(err) {
 			if (err) throw err;
-			console.log(JSON.stringify(user));
 			db.close();
-		})
+	})
 
 })
 

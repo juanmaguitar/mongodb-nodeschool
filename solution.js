c8cb9ce47e171d5e60f7dea3de71642e8bb0536a
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-08-10 19:45:27 +0200

    FIND PROJECT

--- solution.js
+++ solution.js
@@ -5,7 +5,9 @@ var url = 'mongodb://localhost:27017/learnyoumongo';
 mongo.connect(url, function(err, db) {
 
 	db.collection('parrots')
-		.find({ age: { $gt: +ageQuery }  })
+		.find(
+			{ age: { $gt: +ageQuery }  },
+			{ name: 1, age: 1, _id: 0 })
 		.toArray(function(err, results) {
 			if (err) throw err;
 			console.log(results);

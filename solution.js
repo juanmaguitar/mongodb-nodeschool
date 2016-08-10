c64602257b296cc7540f4ffdcf6f3c6eccaf5055
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-08-10 20:12:09 +0200

    REMOVE2

--- solution.js
+++ solution.js
@@ -1,13 +1,14 @@
 var MongoClient = require('mongodb').MongoClient
 var dbName = process.argv[2];
+var collectionName = process.argv[3];
+var idToRemove = process.argv[4];
 var url = 'mongodb://localhost:27017/' + dbName;
 
 MongoClient.connect(url, function(err, db) {
 	if (err) throw err;
-	db.collection('users')
-	.update(
-		{ username: "tinatime" },
-		{ $set: { age: 40 } },
+	db.collection(collectionName)
+	.remove(
+		{ _id: idToRemove },
 		function(err) {
 			if (err) throw err;
 			db.close();

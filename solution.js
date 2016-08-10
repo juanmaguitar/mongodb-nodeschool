66ecbff51bdd1435bff717a0115338be50ffca4d
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-08-10 20:16:02 +0200

    COUNT

--- solution.js
+++ solution.js
@@ -1,18 +1,17 @@
 var MongoClient = require('mongodb').MongoClient
-var dbName = process.argv[2];
-var collectionName = process.argv[3];
-var idToRemove = process.argv[4];
-var url = 'mongodb://localhost:27017/' + dbName;
+var ageQuery = process.argv[2];
+var url = 'mongodb://localhost:27017/learnyoumongo';
 
 MongoClient.connect(url, function(err, db) {
 	if (err) throw err;
-	db.collection(collectionName)
-	.remove(
-		{ _id: idToRemove },
-		function(err) {
-			if (err) throw err;
-			db.close();
-	})
+	db.collection("parrots")
+		.count(
+			{ age: { $gt: +ageQuery }  },
+			function(err, count) {
+				if (err) throw err;
+				console.log(count);
+				db.close();
+		})
 
 })
 

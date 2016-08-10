f0a2d9746194e14ffcc1cecba2255c5ead8d5ea9
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-08-10 20:37:06 +0200

    AGGREGATE

--- solution.js
+++ solution.js
@@ -1,16 +1,22 @@
 var MongoClient = require('mongodb').MongoClient
-var ageQuery = process.argv[2];
+var sizeQuery = process.argv[2];
 var url = 'mongodb://localhost:27017/learnyoumongo';
 
 MongoClient.connect(url, function(err, db) {
 	if (err) throw err;
-	db.collection("parrots")
-		.count(
-			{ age: { $gt: +ageQuery }  },
-			function(err, count) {
-				if (err) throw err;
-				console.log(count);
-				db.close();
+	db.collection("prices")
+		.aggregate([
+			{ $match: { size: sizeQuery } },
+			{ $group: {
+ 				_id: 'average',
+				average: { $avg: '$price' }
+			}}
+		]).toArray(function(err, results) {
+			if (err) throw err;
+			if (!results.length) throw new Error('No results found')
+			var oResult = results[0];
+			console.log( Number(oResult.average).toFixed(2) );
+			db.close();
 		})
 
 })
